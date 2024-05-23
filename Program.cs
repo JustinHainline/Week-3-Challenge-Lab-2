7d3182558745cc1d9d9dc5bf59770b20d2a3d812
Justin Hainline <Justin.Hainline@mssa.newhorizons.com>
2024-05-23 15:04:28 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace Week_3_Challenge_Lab_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a string containing letters and numbers: ");
+            string myString = Console.ReadLine();
+         
+            double sum = 0;
+
+            for(int i = 0; i < myString.Length; i++)
+            {
+               double number = Char.GetNumericValue(myString, i);
+                if (number != -1)
+                {
+                    sum = sum + number;
+                }
+            }
+            Console.WriteLine($"The total of the numeric digits is {sum}");
+
+        }
+
+      
+
+    }
+}
